0880fa9afdcfb6535759042080e5e4dba7c514ff
flyingBridzz <1390988093@qq.com>
2019-10-25 15:58:58 +0800

    没有修改

--- IAP/APP_LED/USER/main.c
+++ IAP/APP_LED/USER/main.c
@@ -22,7 +22,7 @@ int main(void)
   HAL_Init();                    	//初始化HAL库    
   Stm32_Clock_Init(336,8,2,7);  	//设置时钟,168Mhz
 	delay_init(168);               	//初始化延时函数
-	LED_Init();						//初始化LED	
+	LED_Init();											//初始化LED	
 	
 	while(1)
 	{
